70f763d6f4aef47f5d30c5ff0269398ef8e0d3e2
AMGsmert69 <amgsmart69@gmail.com>
2023-12-07 21:46:18 +0200

    edited files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from "./pages/home.js"
+import Htmlcourse from "./pages/htmlCourse.js"
+import Csscourse from "./pages/cssCourse.js"
+import Javascriptcourse from "./pages/javascriptCourse.js"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1 style={{ color: 'red' }}>Oops!</h1>
+  },
+
+  {
+    path: "/htmlCourse",
+    element: <Htmlcourse />,
+    errorElement: <h1 style={{ color: 'red' }}>Oops!</h1>
+  },
+
+  {
+    path: "/cssCourse",
+    element: <Csscourse />,
+    errorElement: <h1 style={{ color: 'red' }}>Oops!</h1>
+  },
+
+  {
+    path: "/javascriptCourse",
+    element: <Javascriptcourse />,
+    errorElement: <h1 style={{ color: 'red' }}>Oops!</h1>
+  },
+]);
  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
